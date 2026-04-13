fbafefb0277722770eedad9e4202a791c8e73ad2
Iceflower <iceflower@gmx.de>
2026-04-13 17:08:17 +0200

    [dev] Update devcontainer setup

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
+set -ex
 
-echo "Downloading Go module dependencies"
+# download go modules
 go mod download
 cd golangci-plugin
 go mod download
 cd ../
 
+# install golangci-lint and build custom golangci-lint
 echo "Installing golangci-lint"
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/HEAD/install.sh | sh -s -- -b $(go env GOPATH)/bin
 echo "Building custom golangci-lint"
 golangci-lint custom --destination "/home/vscode/.local/bin" --name golangci-lint
 rm $(go env GOPATH)/bin/golangci-lint
 
-echo "Install OS Packages"
+# install OS packages
 sudo apt update
 sudo apt install -y graphviz
